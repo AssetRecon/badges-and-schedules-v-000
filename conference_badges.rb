c9954483479ec6e134f8f5f5bda758a3e8ddd0b3
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 17:57:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+badge_messages = []
+  array.each do |name|
+    badge_messages <<  badge_maker(name)
+  end
+  return badge_messages
+end
